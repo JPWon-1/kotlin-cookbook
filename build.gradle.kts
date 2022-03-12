3bc13e610925496da2838e1e21ae56827db6adc3
원종필 <rocknzero1@gmail.com>
2022-03-12 18:57:35 +0900

    add gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,15 @@ repositories {
 }
 
 dependencies {
+    implementation("org.mockito:mockito-core:4.3.1")
+    implementation("org.mockito:mockito-all:1.10.19")
+    implementation("org.easymock:easymock:4.3")
+    implementation("io.mockk:mockk:1.12.3")
     testImplementation(kotlin("test"))
     testImplementation("org.assertj:assertj-core:3.22.0")
+    // https://mvnrepository.com/artifact/org.mockito.kotlin/mockito-kotlin
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.0.0")
+
 }
 
 tasks.test {
